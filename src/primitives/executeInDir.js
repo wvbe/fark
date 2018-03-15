eed30069faaf2a11630eaf7be65075105e2ee507
wvbe <wybe@x-54.com>
2018-03-16 00:37:02 +0100

    Clean up executeInDir a bit

--- src/primitives/executeInDir.js
+++ src/primitives/executeInDir.js
@@ -2,47 +2,42 @@
 
 const spawn = require('cross-spawn-async');
 
-module.exports = (cwd, argv, log) => {
-	const logAllTheThings = (allTheThings, time) => {
-		log.caption(`${cwd}`);
-
-		allTheThings.forEach(thing => {
-			thing.data.split('\n').forEach(log[thing.type].bind(log));
-		});
-
-		log.debug(`Exited after ${time} ms`);
-	};
-
+module.exports = (cwd, argv) => {
 	return new Promise((resolve, reject) => {
 		const processMessages = [];
-		const timeStart = new Date().getTime();
-		let cancelled = false;
 		const spawnedProcess = spawn(argv[0], argv.slice(1), { cwd: cwd });
 
-		//if(log) {
-		spawnedProcess.stdout.on('data', (data) => processMessages.push({ type: 'debug',  data: data.toString().trim() }));
-		spawnedProcess.stderr.on('data', (data) => processMessages.push({ type: 'notice', data: data.toString().trim() }));
-		//}
+		spawnedProcess.stdout.on('data', (data) => processMessages.push({
+			time: Date.now(),
+			type: 'stdout',
+			data: data.toString()
+		}));
 
+		spawnedProcess.stderr.on('data', (data) => processMessages.push({
+			time: Date.now(),
+			type: 'stderr',
+			data: data.toString()
+		}));
+
+		let cancelled = false;
 		spawnedProcess.on('error', (error) => {
-			if (log) {
-				processMessages.push({ type: 'error', data: error.message || error.code || 'Process exited with an unknown error' });
-				logAllTheThings(processMessages, new Date().getTime() - timeStart);
-			}
+			processMessages.push({
+				time: Date.now(),
+				type: 'error',
+				data: error.message || error.code || 'Process exited with an unknown error'
+			});
+
 			cancelled = true;
 
-			reject(error);
-
 			spawnedProcess.removeAllListeners();
+
+			reject(error);
 		});
 
 		spawnedProcess.on('close', () => {
 			// If cancelled, do not resolve() because  it was already rejected
 			if(cancelled)
 				return;
-
-			if (log)
-				logAllTheThings(processMessages, new Date().getTime() - timeStart);
 
 			resolve(processMessages);
 		});
