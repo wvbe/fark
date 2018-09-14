4aed552f0f7f6f6fc0cd483abe8eaf9b95370bf8
wvbe <wybe@x-54.com>
2018-09-14 12:08:51 +0200

    Add "has-branch", "has-local-branch" and "has-remote-branch" columns and filters

--- api/defaultInformers/gitBranchInformer.js
+++ api/defaultInformers/gitBranchInformer.js
@@ -1,5 +1,28 @@
 const spawnProcess = require('../../src/primitives/executeInDir');
 const propTypeString = require('../propTypes/string');
+const propTypeBoolean = require('../propTypes/boolean');
+
+function createObjectForBranchOutputLine (line) {
+	if (!line.trim()) {
+		return;
+	}
+
+	if (line.includes('remotes/origin/HEAD')) {
+		return;
+	}
+
+	const isCheckedOut = line.startsWith('* ');
+	const namePieces = line.substr(2).trim().split('/');
+
+	// This is a pretty crude detect, its possible to name local branches as "remotes/derp"
+	const isRemote = namePieces[0] === 'remotes' && namePieces[1] && namePieces[2];
+
+	return {
+		branchName: isRemote ? namePieces.slice(2).join('/') : namePieces.join('/'),
+		remoteName: isRemote ? namePieces[1] : null,
+		isCheckedOut
+	}
+}
 
 module.exports = {
 	name: 'git-branch',
@@ -7,29 +30,53 @@ module.exports = {
 	dependencies: ['system'],
 
 	retrieve: ({ isGit, path }) => isGit ?
-		spawnProcess(path, ['git', 'branch', '--list', '--no-color']).then((messages) => ({
-			gitLocalBranches: messages.reduce((branches, message) => branches
-				.concat(message.data
-					.split('\n')
-					.filter(line => !!line.trim())
-					.map(line => ({
-						name: line.substr(2).trim(),
-						checkedOut: line.indexOf('* ') === 0
-					}))
-				), [])
+		spawnProcess(path, ['git', 'branch', '--list', '--no-color', '--all']).then((messages) => ({
+			gitBranches: messages.reduce((branches, message) => branches
+				.concat(message.data.split('\n')
+				.map(createObjectForBranchOutputLine)
+				.filter(b => !!b)
+			), [])
 		})) :
-		{ gitLocalBranches: [] },
+		{ gitBranches: [] },
 
 	props: [
 		{
 			name: 'git-branch',
 			type: propTypeString,
 			description: 'The branch name that is currently checked out',
-			callback: ({ gitLocalBranches }) => {
-				const current = gitLocalBranches.find(info => info.checkedOut);
-				console.dir(current);
-				return current ? current.name : null;
+			callback: ({ gitBranches }) => {
+				const current = gitBranches.find(info => info.isCheckedOut);
+				return current ? current.branchName : null;
 			}
+		},
+		{
+			name: 'has-local-branch',
+			type: propTypeBoolean,
+			isFilterable: true,
+			description: 'Assert wether $1 is a branch on the local machine',
+			callback: ({ gitBranches }, branchName) => gitBranches.find(info => (
+				!info.remoteName &&
+				info.branchName === branchName
+			))
+		},
+		{
+			name: 'has-remote-branch',
+			type: propTypeBoolean,
+			isFilterable: true,
+			description: 'Assert wether $1 is a branch on any of the remotes',
+			callback: ({ gitBranches }, branchName) => gitBranches.find(info => (
+				!!info.remoteName &&
+				info.branchName === branchName
+			))
+		},
+		{
+			name: 'has-branch',
+			type: propTypeBoolean,
+			isFilterable: true,
+			description: 'Assert wether $1 is a branch on the local machine or any of the remotes',
+			callback: ({ gitBranches }, branchName) => gitBranches.find(info => (
+				info.branchName === branchName
+			))
 		}
 	]
 };
